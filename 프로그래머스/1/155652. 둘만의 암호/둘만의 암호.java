aed29a0555803ad8ac0dca6dc185af05f5a0f1a5
dongschiken <138917930+dongschiken@users.noreply.github.com>
2024-03-26 08:11:42 +0900

    [level 1] Title: 둘만의 암호, Time: 12.07 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,34 @@
+class Solution {
+           
+    public String solution(String s, String skip, int index) {
+            String answer = "";
+	        int sLength = s.length();
+	        char[] s_char = new char[sLength];
+	        
+	        for (int i = 0; i < s_char.length; i++) {
+				s_char[i] = s.charAt(i);
+			}
+	        // s의 길이만큼 for문
+	        // index만큼 for 문을 ++돌리다가 skip문자가 나오면 skip
+	        // z가 나오면 --26
+	        for (int i = 0; i < sLength; i++) {
+				for (int j = 0; j < index; j++) {
+					s_char[i] = (char)(s_char[i]+1);
+					if(skip.contains(s_char[i] + "") ) {
+						j--;
+					}
+					if( s_char[i] == '{') {
+						s_char[i] = (char) (s_char[i]-26);
+                        if(skip.contains(s_char[i] + "")){
+                            j--;
+                        }
+					}				
+				}
+			}
+	        
+	       for (int i = 0; i < s_char.length; i++) {
+			answer += ""+s_char[i];
+		}
+	        return answer;
+	    }
+    }
