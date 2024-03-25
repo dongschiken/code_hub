6761d5047f1c97df2323d8cd2afd3c360f505cd4
dongschiken <138917930+dongschiken@users.noreply.github.com>
2024-03-26 08:14:52 +0900

    Update 둘만의 암호.java

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -12,17 +12,18 @@ class Solution {
 	        // index만큼 for 문을 ++돌리다가 skip문자가 나오면 skip
 	        // z가 나오면 --26
 	        for (int i = 0; i < sLength; i++) {
-				for (int j = 0; j < index; j++) {
-					s_char[i] = (char)(s_char[i]+1);
-					if(skip.contains(s_char[i] + "") ) {
-						j--;
-					}
-					if( s_char[i] == '{') {
-						s_char[i] = (char) (s_char[i]-26);
-                        if(skip.contains(s_char[i] + "")){
-                            j--;
-                        }
-					}				
+			for (int j = 0; j < index; j++) {
+				s_char[i] = (char)(s_char[i]+1);
+				if(skip.contains(s_char[i] + "") ) {
+					j--;
+				}
+				if( s_char[i] == '{') {
+					s_char[i] = (char) (s_char[i]-26);
+					// 만약 skip에 a가 포함되어 있다면 그때도 j를 -- 해야한다.
+		                	if(skip.contains(s_char[i] + "")){
+		                            j--;
+		                        }
+				}				
 				}
 			}
 	        
