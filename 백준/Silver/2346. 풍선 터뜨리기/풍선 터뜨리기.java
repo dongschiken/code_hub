ec4c2e2991e48ba405c7ea93f88e1c76dbcc1379
dongschiken <138917930+dongschiken@users.noreply.github.com>
2024-03-31 12:06:53 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<int[]> balloons = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            balloons.add(new int[]{i, Integer.parseInt(st.nextToken())});
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        for (int i = 0; i < N; i++) {
+            int[] current = balloons.get(index);
+            sb.append(current[0]).append(" ");
+            int move = current[1];
+            balloons.remove(index);
+
+            if (balloons.isEmpty()) break;
+
+            
+            if (move > 0) {
+                index = (index + move - 1) % balloons.size();
+            } else {
+                index = (index + move) % balloons.size();
+                if (index < 0) {
+                    index += balloons.size();
+                }
+            }
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
+//10
+//3 2 1 -3 -1 4 -2 -5 5 6
