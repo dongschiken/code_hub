7791616e8c79117f46d67117870c43b5e56500c9
dongschiken <138917930+dongschiken@users.noreply.github.com>
2024-04-03 00:39:42 +0900

    [level 2] Title: 올바른 괄호, Time: 18.74 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,43 +1,37 @@
-import java.util.*;
-class Solution {
-    boolean solution(String s) {
-       
-		boolean answer = true;
-		Stack stack = new Stack();
+import java.util.Stack;
 
-		if(s.charAt(0) == ')'){
-			return  answer = false;
-		}
-		stack.push(s.charAt(0));
-		char c = ' ';
-		int count = 1;
-		while(count <= s.length()-1){
-			if( stack.isEmpty()) {
-				stack.push(s.charAt(count));
-				count++;
-				continue;
-			}
-			c = (char)stack.pop();
-			if(c == '('){
-				if( s.charAt(count) == ')') {
-					count++;
-					continue;
-				}else {
-					stack.push(c);
-					stack.push(s.charAt(count));
-				}
-					
+class Solution {
+    
+    
+    boolean solution(String s) {
+        boolean answer = true;
+
+      Stack st = new Stack();
+        int length = s.length();
+        for (int i = 0; i <length; i++) {
+        	
+        	if (s.charAt(i)=='(') {
+        		st.push(s.charAt(i));        	
 				
-			}else {
-				stack.push(c);
-				stack.push(s.charAt(count));
+			} else if(s.charAt(i)==')'){
+				if (st.isEmpty()) {
+					
+				answer = false;	
+				}
+							
+				if (!st.empty()) {
+					
+					st.pop();				
+				}
+				
 			}
-			count++;
+
+		}//for
+        System.out.println(st);
+        if (!st.isEmpty()) {
+			answer = false;
 		}
-		if( stack.isEmpty()) {
-			return answer = true;
-		}else {
-			return answer = false; 
-		}
-  }
+
+        return answer;
+    }
 }
