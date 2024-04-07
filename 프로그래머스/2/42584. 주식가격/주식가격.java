57559ece58438ebdef370d1e481c3b76f260652b
dongschiken <138917930+dongschiken@users.noreply.github.com>
2024-04-07 19:58:06 +0900

    [level 2] Title: 주식가격, Time: 26.42 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+     ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
+		int[] answer = new int[prices.length];
+		for (int i = 0; i < prices.length; i++) {
+			deque.offer(prices[i]);
+		}
+		OUT :
+		for (int i = 0; i < prices.length; i++) {
+			int count = 0;
+			int now_price = deque.poll();
+			for (int j = 0; j < deque.size(); j++) {
+				count++;
+				if( prices[j+i+1] >= now_price ) {
+					answer[i] = count;			
+					continue;
+				}else if( prices[j+i+1] < now_price) {
+					answer[i] = count;		
+					continue OUT;
+				}
+			}
+		}
+		return answer;
+	}
+}
