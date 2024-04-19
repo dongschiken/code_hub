1e69924b98fbfc668355de4dbff964a45553c872
동영 <138917930+dongschiken@users.noreply.github.com>
2024-04-19 23:34:37 +0900

    [Silver III] Title: 칸토어 집합, Time: 376 ms, Memory: 66648 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,45 +1,56 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	
+	static char[] len;
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		StringBuilder sb;
+		while ( ( input = br.readLine()) != null ) {
+			sb = new StringBuilder();
+			int temp = Integer.parseInt(input);
+			int length = (int)Math.pow(3, temp);
+			
+			len = new char[length];
+			for (int i = 0; i < length; i++) {
+				len[i] = '-';
+			}
+			
+			int start = 0;
+			recursion_canto( start , length);
+			
+			for (int i = 0; i < length; i++) {
+				sb.append(len[i]+"");
+			}
+			System.out.println(sb);
+		}
+		
+	}
+	
+	private static void recursion_canto(int start, int length) {
+		
+		if( length <= 1 ) return;
+		
+		// length를 3등분
+		int canto_length = length/3;
+		
+		// 9~17까지 공백으로 바꿔야해서 공백으로 바꾸는것은 시작점+길이 ~ 길이*2한 값이 되는것이다.
+		// 27일 경우 9 9 9 로 3등분 되고 시작이 0+9 ~ 9*2라서 len[9]~len[17]이 공백으로 바뀌는 것이다.
+		// 이과정을 계속 반복하면 된다.
+		for (int i = start+canto_length; i < start+canto_length*2; i++) {
+			len[i] = ' ';
+			
+		}
+		
 
+		recursion_canto(start, canto_length);
+		recursion_canto(start+canto_length*2, canto_length);
+	}
 
-
-    static char[] len;
-
-    public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String input = "";
-    while( (input = br.readLine()) != null ){
-        StringBuilder sb = new StringBuilder();
-        int input_int = Integer.parseInt(input);
-        int length = (int)Math.pow(3, input_int);
-        len = new char[length];
-
-        for (int i = 0; i < length; i++){
-            len[i] = '-';
-        }
-
-        recursive_canto(0, length);
-
-
-        for (int i = 0; i < length; i++){
-            sb.append(len[i]+"");
-        }
-        System.out.println(sb);
-       }
-    }
-    public static void recursive_canto(int start, int length){
-        if( length == 1)return;
-        int newLength = length/3;
-        // 9 9 9
-        // start 0 length 9
-        //
-        for (int i = start+newLength; i < start+newLength*2; i++){
-            len[i] = ' ';
-        }
-        recursive_canto(start, newLength);
-        recursive_canto( start+newLength*2, newLength);
-    }
 }
