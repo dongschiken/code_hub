3f7e6f59b922f7ac427003976d54cad02dfc7190
동영 <138917930+dongschiken@users.noreply.github.com>
2024-04-20 00:00:51 +0900

    [Silver III] Title: 칸토어 집합, Time: 304 ms, Memory: 21732 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,36 +1,33 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.*;
+import java.io.InputStreamReader;
 
 public class Main {
 	
-	static char[] len;
+	static StringBuilder sb;
 	
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String input;
-		StringBuilder sb;
-		while ( ( input = br.readLine()) != null ) {
+		while ( (input = br.readLine()) != null ) {
 			sb = new StringBuilder();
 			int temp = Integer.parseInt(input);
 			int length = (int)Math.pow(3, temp);
 			
-			len = new char[length];
+			
 			for (int i = 0; i < length; i++) {
-				len[i] = '-';
+				sb.append('-');
 			}
 			
 			int start = 0;
 			recursion_canto( start , length);
 			
-			for (int i = 0; i < length; i++) {
-				bw.write(len[i]+"");
-			}
-			bw.write("\n");
+			System.out.println(sb);
 			
 		}
-		bw.flush();
+		
 	}
 	
 	private static void recursion_canto(int start, int length) {
@@ -44,7 +41,7 @@ public class Main {
 		// 27일 경우 9 9 9 로 3등분 되고 시작이 0+9 ~ 9*2라서 len[9]~len[17]이 공백으로 바뀌는 것이다.
 		// 이과정을 계속 반복하면 된다.
 		for (int i = start+canto_length; i < start+canto_length*2; i++) {
-			len[i] = ' ';
+			sb.setCharAt(i,' ');
 			
 		}
 		
