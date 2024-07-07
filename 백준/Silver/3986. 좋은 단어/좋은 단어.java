2fb533920f8b2e84bf12fc25b3a94d3fe767cd34
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-07-08 01:07:47 +0900

    [Silver IV] Title: 좋은 단어, Time: 340 ms, Memory: 20788 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -1,40 +1,35 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException, NumberFormatException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		Stack<String> stack;
-		int N = Integer.parseInt(br.readLine());
+		Stack<Character> stack = new Stack<Character>();
 		int count = 0;
+		int N = Integer.parseInt(br.readLine());
 		for (int i = 0; i < N; i++) {
-			stack = new Stack<String>();
-			String[] temp = br.readLine().split("");
-			int t_length = 0;
-			while (t_length < temp.length) {
-				stack.push(temp[t_length]);
-				//System.out.println(stack);
-				if( stack.size() >= 2) {
-					String str1 = stack.pop();
-					String str2 = stack.pop();
-					if(str1.equals(str2)) {
-						t_length++;
-						continue;
-					}else {
-						stack.push(str2);
-						stack.push(str1);
+			
+			char[] temp = br.readLine().toCharArray();
+			int c_length = 0;
+			while (c_length < temp.length) {
+				stack.push(temp[c_length]);
+				if(stack.size() >= 2) {
+					char c1 = stack.pop();
+					char c2 = stack.pop();
+					if(c1 != c2) {
+						stack.push(c2);
+						stack.push(c1);
 					}
 				}
-				t_length++;
+				c_length++;
 			}
-			if( stack.isEmpty() ) {
-				count++;
-			}
+			if(stack.isEmpty()) count++;
+			stack.clear();
 		}
 		bw.write(count+"");
 		bw.flush();
 	}
+
 }
