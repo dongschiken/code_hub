e03d8f2c8d95c9d7bb5522b6bc82dd748b0625f8
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-07-12 23:32:29 +0900

    [Silver III] Title: 수열, Time: 256 ms, Memory: 24408 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,36 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException, NumberFormatException {
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		
+
 		int[] arr = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		// 9
-		// 6
-		// N - K + 1
-		int max = Integer.MIN_VALUE;
-		int temp = 0;
-		for (int i = 0; i < N - K + 1; i++) {
-			for (int j = 0; j < K; j++) {
-				temp += arr[j+i];
-			}
-			max = Math.max(max, temp);
-			temp = 0;
+
+		// Initial sum of the first K elements
+		int sum = 0;
+		for (int i = 0; i < K; i++) {
+			sum += arr[i];
 		}
-		bw.write(max+"");
+		int max = sum;
+
+		// Sliding window: subtract the element that's left and add the new element
+		for (int i = K; i < N; i++) {
+			sum += arr[i] - arr[i - K];
+			if (sum > max) {
+				max = sum;
+			}
+		}
+
+		bw.write(max + "\n");
 		bw.flush();
 		bw.close();
 	}
-
 }
