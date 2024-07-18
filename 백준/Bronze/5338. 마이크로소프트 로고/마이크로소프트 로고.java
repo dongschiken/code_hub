c940e979ba775eab808bd488ec67d15b0bb22fd3
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-07-19 00:29:54 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 120 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	   String str1 = "       _.-;;-._";
+	   String str2 = "'-..-'|   ||   |";
+	   String str3 = "'-..-'|_.-;;-._|";
+	   String str4 = "'-..-'|   ||   |";   
+	   String str5 = "'-..-'|_.-''-._|";
+	   System.out.println(str1);
+	   System.out.println(str2);
+	   System.out.println(str3);
+	   System.out.println(str4);
+	   System.out.println(str5);
+	   
+	      
+	}
+
+}
