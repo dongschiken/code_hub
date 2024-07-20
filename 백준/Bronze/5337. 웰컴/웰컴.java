8549c71b39f448709075cc533bc7f74657f2897b
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-07-21 01:33:50 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		String str1 = ".  .   .";
+		String str2 = "|  | _ | _. _ ._ _  _";
+		String str3 = "|/\\|(/.|(_.(_)[ | )(/.";
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+
+}
