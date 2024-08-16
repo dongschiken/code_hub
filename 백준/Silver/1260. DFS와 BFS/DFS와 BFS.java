24490d0f9d83aa32fde87cb7377c574ab0339e9d
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-08-17 00:17:02 +0900

    [Silver II] Title: DFS와 BFS, Time: 268 ms, Memory: 24376 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -10,63 +9,67 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
+	static int[][] grape;
+	static int[] visited;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static int[][] grape;
-	static boolean[] visited;
-	static Queue<Integer> queue = new ArrayDeque<Integer>();
-	public static void main(String[] args) throws IOException, NumberFormatException{
+	
+	public static void main(String[] args) throws IOException {
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int nodeCount = Integer.parseInt(st.nextToken());
-		int lineCount = Integer.parseInt(st.nextToken());
-		int v = Integer.parseInt(st.nextToken());
 		
-		grape = new int[nodeCount+1][nodeCount+1]; 
-		visited = new boolean[nodeCount+1];
-		for (int i = 0; i < lineCount; i++) {
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		
+		grape = new int[N+1][N+1];
+		
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int one = Integer.parseInt(st.nextToken());
-			int two = Integer.parseInt(st.nextToken());
-			grape[one][two] = 1;
-			grape[two][one] = 1;
+			int left = Integer.parseInt(st.nextToken());
+			int right = Integer.parseInt(st.nextToken());
+			
+			grape[left][right] = 1;
+			grape[right][left] = 1;
 		}
-		
-		dfs(v);
-		bw.flush();
+		visited = new int[N+1];
+		dfs(S);
 		bw.write("\n");
-		visited = new boolean[nodeCount+1];
-		visited[v] = true;
-		queue.offer(v);
-		bfs();
+		visited = new int[N+1];
+		bfs(S);
 		bw.flush();
-
+		
 	}
-	private static void bfs() throws IOException {
+
+	private static void bfs(int s) throws IOException {
+		Queue<Integer> queue = new ArrayDeque<>();
+		
+		queue.offer(s);
+		visited[s] = 1;
 		while (!queue.isEmpty()) {
+			
 			int cur = queue.poll();
-			bw.write(cur +" ");
-			for (int i = 0; i < visited.length; i++) {
-				if( grape[cur][i] == 1 && !visited[i]) {
-					visited[i] = true;
+			bw.write(cur+" ");
+			for (int i = 1; i < grape[cur].length; i++) {
+				if(visited[i] == 0 && grape[cur][i] == 1) {
+					visited[i] = 1;
 					queue.offer(i);
 				}
 			}
+			
 		}
 		
-		
 	}
-	private static void dfs(int v) throws IOException {
+
+	private static void dfs(int node) throws IOException {
 		
-		visited[v] = true;
-		bw.write(v + " ");
-		for (int i = 1; i < visited.length; i++) {
-			if(grape[v][i] == 1 && !visited[i]) {
+		visited[node] = 1;
+		bw.write(node+" ");
+		for (int i = 1; i < grape[node].length; i++) {
+			if(visited[i] == 0 && grape[node][i] == 1) {
 				dfs(i);
 			}
 		}
 		
 	}
-	
-	
-
 }
