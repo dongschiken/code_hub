3447052f8c389afb87c82c623df0f319a9d9bb45
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-08-17 01:06:21 +0900

    [Silver I] Title: 단지번호붙이기, Time: 128 ms, Memory: 16268 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -3,60 +3,80 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
 
 public class Main {
-	
-	static int[] cooX = new int[] {1, -1, 0, 0};
-	static int[] cooY = new int[] {0, 0, 1, -1};
-	static int count = 0;
+
+	static boolean[][] visited;
+	static int[][] grape;
+	static List<Integer> list = new ArrayList<>();
+	static int[] dr = {1, -1, 0, 0};
+	static int[] dc = {0, 0, 1, -1};
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int node = Integer.parseInt(br.readLine());
-		int[][] grape = new int[node+2][node+2];
-		int[][] visited = new int[node+2][node+2];
-		ArrayList<Integer> danji = new ArrayList<Integer>();
-		for (int i = 1; i <= node; i++) {
-			String[] temp = br.readLine().split("");
-			for (int j = 1; j <= node; j++) {
-				grape[i][j] = Integer.parseInt(temp[j-1]);
-			}
-		}
-		br.close();
-		int danjiCount = 0;
-		for (int i = 1; i <= node; i++) {
-			for (int j = 1; j <= node; j++) {
-				if( grape[i][j] == 1 && visited[i][j] != 1) {
-					dfs(i, j, grape, visited);
-					danjiCount++;
-					danji.add(count);
-					count = 0;
+
+		int N = Integer.parseInt(br.readLine());
+		grape = new int[N][N];
+		visited = new boolean[N][N];
+
+		for (int i = 0; i < N; i++) {
+			grape[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+		} 
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(!visited[i][j] && grape[i][j] == 1) {
+					bfs(i, j);
+					cnt++;
 				}
 			}
 		}
-		bw.write(danjiCount+"\n");
-		Collections.sort(danji);
-		for (int i = 0; i < danjiCount; i++) {
-			bw.write(danji.get(i)+"\n");
+		bw.write(cnt+"\n");
+		Collections.sort(list);
+		for (int i = 0; i < list.size(); i++) {
+			bw.write(list.get(i)+"\n");
 		}
 		bw.flush();
 		bw.close();
+		br.close();
 	}
-	private static void dfs(int x, int y, int[][] grape, int[][] visited) {
-		visited[x][y] = 1;
-		count++;
-		for (int i = 0; i < 4; i++) {
-			int newX = x + cooX[i];
-			int newY = y + cooY[i];
-			if(grape[newX][newY] == 1 && visited[newX][newY] != 1) {
-				dfs(newX, newY, grape, visited);
+
+	private static void bfs(int i, int j) {
+		Queue<int[]> queue = new ArrayDeque<>();
+		int cnt = 0;
+		queue.offer(new int[] {i, j});
+		visited[i][j] = true;
+		while (!queue.isEmpty()) {
+
+			int[] temp = queue.poll();
+			int r = temp[0];
+			int c = temp[1];
+			cnt++;
+			for (int k = 0; k < 4; k++) {
+
+				int nr = r + dr[k];
+				int nc = c + dc[k];
+
+				if(check(nr, nc) && !visited[nr][nc] && grape[nr][nc] == 1) {
+					visited[nr][nc] = true;
+					queue.offer(new int[] {nr, nc});
+
+				}
 			}
 		}
-		
+		list.add(cnt);
 	}
 
+	private static boolean check(int nr, int nc) {
+		boolean check = false;
+		if(nr >= 0 && nr < grape.length && nc >= 0 && nc < grape.length) check = true;
+		return check;
+	}
 }
-
