53c15613d25e184ae0356cf64dbd31ca9fc75810
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-08-16 23:33:58 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,42 +1,51 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main{
-    
-    private static int[][] virus;
-    private static boolean[] check;
-    private static int count = 0;
-    private static int com_node;
-    private static int line;
-    
-    public static void main(String[] args) throws IOException, NumberFormatException {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        com_node = Integer.parseInt(br.readLine());
-        line = Integer.parseInt(br.readLine());
-        
-        virus = new int[com_node+1][com_node+1];
-        check = new boolean[com_node+1];
-        
-        StringTokenizer st;
-        for(int i = 0; i < line; i++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end   = Integer.parseInt(st.nextToken());
-            virus[start][end] = virus[end][start] = 1;
-        }
-        bfs(1);
-        System.out.println(count-1);
-    }
-    public static void bfs(int start){
-        check[start] = true;
-        count++;
-        
-        for(int i = 0; i <= com_node; i++){
-            if(virus[start][i] == 1 && !check[i]){
-                bfs(i);
-            }
-        }
-    }
+public class Main {
+	
+	static int[][] grape;
+	static int[] visited;
+	// 바이러스 감염된 컴퓨터 카운트
+	static int cnt;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		grape = new int[N+1][N+1];
+		visited = new int[N+1];
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int left = Integer.parseInt(st.nextToken());
+			int right = Integer.parseInt(st.nextToken());
+			
+			grape[left][right] = 1;
+			grape[right][left] = 1;
+		}
+		visited[1] = 1;
+		dfs(1);
+		System.out.println(cnt);
+	}
+
+	private static void dfs(int node) {
+		
+
+		for (int i = 0; i < grape[node].length; i++) {
+			if(visited[i] == 0 && grape[node][i] == 1) {
+				cnt++;
+				visited[i] = 1;
+				dfs(i);
+			}
+		}
+		
+	}
 }
