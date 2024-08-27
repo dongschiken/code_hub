cea41c2cbb204697f175e31168763e378ca178d2
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-08-28 03:23:36 +0900

    [Gold V] Title: 치킨 배달, Time: 212 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int[] dr = {1, -1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static List<int[]> list = new ArrayList<>();
+	static List<int[]> temp = new ArrayList<>();
+	static int[][] visited;
+	static int[][] map;
+	static int min = Integer.MAX_VALUE;
+	static int result;
+	static int N, M;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 2) list.add(new int[] {i, j});
+				if(map[i][j] == 1) temp.add(new int[] {i, j});
+			}
+		}
+		dfs(0, 0);
+		System.out.println(min);
+	}
+
+	private static void dfs(int loop, int stage) {
+		if(stage == M) {
+			distance();
+			return;
+		}
+		
+		for (int i = loop; i < list.size(); i++) {
+			int r = list.get(i)[0];
+			int c = list.get(i)[1];
+			map[r][c] = 3;
+			dfs(i + 1, stage + 1);
+			map[r][c] = 2;
+		}
+	}
+
+	private static void distance() {
+		// 좌표값으로 거리 구하기
+		// Math.abs(r1 - r2) + Math.abs(c1 - c2)
+		List<int[]> chickens = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(map[i][j] == 3) chickens.add(new int[] {i, j});
+			}
+		}
+		int sum = 0;
+		int result = 0;
+		for (int i = 0; i < temp.size(); i++) {
+			int min = Integer.MAX_VALUE;
+			for (int j = 0; j < chickens.size(); j++) {
+				int[] chicken = chickens.get(j);
+				sum = (Math.abs(chicken[0] - temp.get(i)[0]) + Math.abs(chicken[1] - temp.get(i)[1]));
+				min = Math.min(min, sum);
+			}
+			result += min;
+		}
+		min = Math.min(min, result);
+	}
+}
+
