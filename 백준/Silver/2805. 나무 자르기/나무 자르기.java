e84864a46389449343d423f45867fc4f0da4b052
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-09-10 00:57:31 +0900

    [Silver II] Title: 나무 자르기, Time: 472 ms, Memory: 123308 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,48 +1,42 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int tree_count = (int)Long.parseLong(st.nextToken());
-        long my_tree = Long.parseLong(st.nextToken());
-
-        long[] tree_arr = new long[tree_count];
-        long max = 0;
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < tree_count; i++){
-            tree_arr[i] = Long.parseLong(st.nextToken());
-            if( max < tree_arr[i] ){
-                max = tree_arr[i];
-            }
-        }
-
-        max++;
-        long min = 0;
-        long mid = 0;
-
-        while ( max > min ){
-
-            mid = (max+min) / 2;
-            long number = 0;
-            long temp = 0 ;
-            for (int i = 0; i < tree_count; i++){
-                if( (temp = tree_arr[i] - mid) > 0){
-                    number += temp;
-                }
-            }
-
-
-            if( number >= my_tree ){
-                min = mid+1;
-            }else{
-                max = mid;
-            }
-        }
-        System.out.println(max-1);
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		long[] arr = new long[N];
+		long max = -1;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(arr[i], max);
+		}
+		long start = 1;
+		long end = max;
+		while (start <= end) {
+			
+			long mid = (start + end) / 2;
+			long length = 0;
+			for (int i = 0; i < N; i++) {
+				if(arr[i] - mid < 0) continue;
+				length += arr[i] - mid;
+			}
+			if(length < K) {
+				end = mid - 1;
+			}else {
+				start = mid + 1;
+			}
+		}
+		System.out.println(start - 1);
+	}
 }
