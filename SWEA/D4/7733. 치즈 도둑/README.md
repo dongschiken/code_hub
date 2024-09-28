7a6b21b8db992f44ede66eb3f9a55f88dd947dc2
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-09-28 17:28:29 +0900

    [D4] Title: 치즈 도둑, Time: 344 ms, Memory: 90,840 KB -BaekjoonHub

---
+++ SWEA/D4/7733. 치즈 도둑/README.md
@@ -0,0 +1,15 @@
+# [D4] 치즈 도둑 - 7733 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWrDOdQqRCUDFARG) 
+
+### 성능 요약
+
+메모리: 90,840 KB, 시간: 344 ms, 코드길이: 2,294 Bytes
+
+### 제출 일자
+
+2024-09-28 17:28
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -0,0 +1,104 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	static int[] dr = {1, -1, 0, 0};
+	static int[] dc = {0, 0, 1, -1};
+	static boolean[][] visited;
+	static Queue<Node> q;
+	static int[][] map;
+	static int N;
+	static class Node {
+		int r, c;
+
+		public Node(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
+
+		public Node() {
+			super();
+		}
+
+		@Override
+		public String toString() {
+			return "Node [r=" + r + ", c=" + c + "]";
+		}
+		
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= T; i++) {
+			N = Integer.parseInt(br.readLine());
+			map = new int[N][N];
+
+			int max = Integer.MIN_VALUE;
+			for (int j = 0; j < N; j++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j2 = 0; j2 < N; j2++) {
+					map[j][j2] = Integer.parseInt(st.nextToken());
+					max = Math.max(map[j][j2], max);
+				}
+			}
+			int maxValue = -1;
+			for (int j = 0; j <= max; j++) {
+				visited = new boolean[N][N];
+				int cnt = 0;
+				for (int j2 = 0; j2 < N; j2++) {
+					for (int k = 0; k < N; k++) {
+						if(!visited[j2][k] && map[j2][k] > j) {
+							bfs(j2, k, j);
+							cnt++;
+						}
+					}
+				}
+				maxValue = Math.max(cnt, maxValue);
+			}
+			bw.write("#"+i+" "+maxValue+"\n");
+		}
+		bw.flush();
+	}
+
+	private static void bfs(int r, int c, int day) {
+		q = new ArrayDeque<>();
+		visited[r][c] = true;
+		q.offer(new Node(r, c));
+		while (!q.isEmpty()) {
+			Node n = q.poll();
+			r = n.r;
+			c = n.c;
+			
+			for (int i = 0; i < 4; i++) {
+				int nr = r + dr[i];
+				int nc = c + dc[i];
+				
+				if(check(nr, nc)) continue;
+				if(!visited[nr][nc] && map[nr][nc] > day) {
+					q.offer(new Node(nr, nc));
+					visited[nr][nc] = true;
+				}
+			}
+		}
+		
+	}
+
+	private static boolean check(int nr, int nc) {
+		return nr < 0 || nr >= N || nc < 0 || nc >= N;
+	}
+	
+
+}
