5cc27f609821b5a75f44eceaef979c84d9634bf8
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-09-29 21:38:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 122 ms, Memory: 18,716 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -7,29 +7,27 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		int[] price;
-		int[] result;
-		int[] month;
 		int T = Integer.parseInt(br.readLine());
 		for (int i = 1; i <= T; i++) {
+			int[] result = new int[13];
+			int[] price = new int[4];
+			
 			st = new StringTokenizer(br.readLine());
-			result = new int[13];
-			price = new int[4];
-			for (int j = 0; j < 4; j++) {
+			for (int j = 0; j < price.length; j++) {
 				price[j] = Integer.parseInt(st.nextToken());
 			}
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j < 13; j++) {
-				int cost = Integer.parseInt(st.nextToken());
-				result[j] = Math.min(result[j - 1] + cost * price[0], result[j - 1] + price[1]);
-				if(j >= 3) result[j] = Math.min(result[j], result[j - 3] + price[2]);
+				int day = Integer.parseInt(st.nextToken());
+				result[j] = Math.min(result[j - 1] + price[0] * day , result[j - 1] + price[1]);
+				if( j >= 3 ) result[j] = Math.min(result[j], result[j - 3] + price[2]);
 			}
-			bw.write("#"+i+" "+Math.min(result[12], price[3])+"\n");
+			result[12] = Math.min(price[3], result[12]);
+			bw.write("#"+i+" "+result[12]+"\n");
 		}
 		bw.flush();
 		bw.close();
