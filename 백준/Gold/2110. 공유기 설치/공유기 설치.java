8f7b8a7437c9c2246110ef31a2a94970fee95fa2
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-10-02 00:59:53 +0900

    [Gold IV] Title: 공유기 설치, Time: 272 ms, Memory: 29416 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -22,35 +21,35 @@ public class Main {
 			max = Math.max(max, arr[i]);
 		}
 		
-		// 정렬된 상태로 비교로직을 구현할꺼라서
 		Arrays.sort(arr);
-		long start = 1; // 최소거리
-		long end = max; // 매개변수에서 이분탐색을 진행하기 위해서 현재 찾을 수 있는 값중에 제일 큰값을 end로 만든다
+		long start = 1;
+		long end = max;
 		
-		// 이분탐색 로직
 		while (start <= end) {
 			
 			long mid = (start + end) / 2;
 			
 			int cnt = 1;
-			long cur = arr[0]; 	// 무조건 첫번째 위치는 공유기 설치해야한다.
-			// 다음 위치에 현재값을 mid와 더해서 같거나 크면 설치할 수 있음! -> cnt를 늘림
+			long cur = arr[0];
 			for (int i = 1; i < N; i++) {
 				if(arr[i] >= cur + mid) {
 					cnt++;
-					cur = arr[i]; // 현재 집을 다음집 위치로 초기화
+					cur = arr[i];
 				}
 			}
-			// 설치된 개수가 K개보다 작으면 end를 mid쪽으로
-			// 설치된 개수가 K개 또는 K개보다 크면 start를 mid쪽으로
 			if(cnt < K) {
 				end = mid - 1;
 			}else {
 				start = mid + 1;
 			}
 		}
-		// 로직 검사 전부 끝내고 오면 start는 항상 최대로 구할 수 있는 수 + 1이 되어 있어서 -1을 해줘야한다.
 		System.out.println(end);
-		
 	}
 }
+//6 4
+//5
+//17
+//10
+//8
+//22
+//13
