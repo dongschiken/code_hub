475af5ce3cc7badeb88ac21757318018325296ff
dongschicken <138917930+dongschiken@users.noreply.github.com>
2025-01-06 00:08:49 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.18 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -5,21 +5,10 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();    
         Arrays.sort(A);
+        Arrays.sort(B);
         for(int i = 0; i < B.length; i++) {
-            list.add(B[i]);
-        }
-        Collections.sort(list, Comparator.reverseOrder());
-        for(int i = 0; i < B.length; i++) {
-            B[i] = list.get(i);
-        }
-        // 1 2 4
-        // 5 4 4
-        // 4 4 5
-        // 4 2 1
-        for(int i = 0; i < A.length; i++) {
-            answer += B[i] * A[i];    
+            answer += A[i] * B[B.length - i - 1];
         }
         return answer;
     }
