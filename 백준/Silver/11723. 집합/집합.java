2a73b3d2904f00739ba0e9be8d4b46ca427523d4
dongschicken <138917930+dongschiken@users.noreply.github.com>
2025-01-24 00:28:49 +0900

    [Silver V] Title: 집합, Time: 1032 ms, Memory: 313256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,58 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		Map<Integer, Integer> map = new HashMap<>();
-		StringTokenizer st;
-		
-		for(int i = 1; i <= 20; i++) {
-			map.put(i, 0);
-		}
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int t = 0; t < T; t++) {
-			st = new StringTokenizer(br.readLine());
-			switch(st.nextToken()) {
-			case "add":
-				map.put( Integer.parseInt(st.nextToken()), 1);
-				break;
-			case "remove":
-				map.replace( Integer.parseInt(st.nextToken()), 0);
-				break;
-			case "check":
-				int result = map.get(Integer.parseInt(st.nextToken())) == 1 ? 1 : 0;
-				sb.append(result).append("\n");
-				break;
-			case "toggle":
-				int key = Integer.parseInt(st.nextToken());
-				if(map.get(key) == 1) {
-					map.put(key, 0);
-				}else {
-					map.put(key, 1);
-				}
-				break;
-			case "all":
-				for(int i = 1; i <= 20; i++) {
-					map.put(i, 1);
-				}
-				break;
-			case "empty":
-				for(int i = 1; i <= 20; i++) {
-					map.put(i, 0);
-				}
-				break;
-			
-			}
-			}
-		System.out.println(sb);
-		}
-	}
-
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int S = 0;  // 공집합으로 시작
+        int M = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+            
+            switch(op) {
+                case "add":
+                    int x = Integer.parseInt(st.nextToken());
+                    S |= (1 << (x - 1));
+                    break;
+                case "remove":
+                    x = Integer.parseInt(st.nextToken());
+                    S &= ~(1 << (x - 1));
+                    break;
+                case "check":
+                    x = Integer.parseInt(st.nextToken());
+                    sb.append((S & (1 << (x - 1))) != 0 ? 1 : 0).append('\n');
+                    break;
+                case "toggle":
+                    x = Integer.parseInt(st.nextToken());
+                    S ^= (1 << (x - 1));
+                    break;
+                case "all":
+                    S = (1 << 20) - 1;
+                    break;
+                case "empty":
+                    S = 0;
+                    break;
+            }
+        }
+        System.out.print(sb);
+    }
+}
