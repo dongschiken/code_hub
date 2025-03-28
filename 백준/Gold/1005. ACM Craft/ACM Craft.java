e7d1f26efffe75c443d7c772f9db001d5c9db41b
dongschicken <138917930+dongschiken@users.noreply.github.com>
2025-03-28 14:11:17 +0900

    [Gold III] Title: ACM Craft, Time: 736 ms, Memory: 243720 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,42 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
-import org.w3c.dom.Node;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-
+class Main {
   static int[] D;
   static int[] DP;
   static int[] times;
-  static int[] parents;
-  static List<List<Node>> graph;
-  static class Node {
-    int v, p;
-    public Node(int v, int p) {
-      this.v = v;
-      this.p = p;
-    }
-  }
+  static List<List<Integer>> graph;
+
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
     StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+
     int T = Integer.parseInt(br.readLine());
     for(int t = 0; t < T; t++) {
       st = new StringTokenizer(br.readLine());
       int N = Integer.parseInt(st.nextToken());
       int M = Integer.parseInt(st.nextToken());
-      graph = new ArrayList<>();
-      parents = new int[N+1];
-      DP = new int[N+1];
       D = new int[N+1];
+      DP = new int[N+1];
       times = new int[N+1];
-      for(int i = 0 ; i <= N; i++) {
+      graph = new ArrayList<>();
+      for(int i = 0; i <= N; i++) {
         graph.add(new ArrayList<>());
       }
 
@@ -49,36 +35,32 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        graph.get(a).add(new Node(b, 0));
+        graph.get(a).add(b);
         D[b]++;
       }
 
-      Queue<Node> q = new ArrayDeque<>();
-      for(int i = 0; i < D.length; i++) {
-        parents[i] = D[i] + 1;
+      Queue<Integer> q = new ArrayDeque<>();
+      for(int i = 1; i <= N; i++) {
         if(D[i] == 0) {
-          q.offer(new Node(i, 0));
-          DP[i] = Math.max(times[i], DP[0]);
+          q.offer(i);
+          DP[i] = Math.max(DP[0], times[i]);
         }
       }
+
       while(!q.isEmpty()) {
-        Node n = q.poll();
-        int curr = n.v;
-        // 계산
-        // parents이전DP + 현재타임 < parents지금DP
-        for(Node next : graph.get(curr)) {
-          if(DP[curr] + times[next.v] > DP[next.v]) {
-            DP[next.v] = DP[curr] + times[next.v];
+        int curr = q.poll();
+        for(int next : graph.get(curr)) {
+          D[next]--;
+          if(DP[next] < DP[curr] + times[next]) {
+            DP[next] = DP[curr] + times[next];
           }
-          D[next.v]--;
-          if(D[next.v] == 0) {
-            q.offer(new Node(next.v, 0));
+          if(D[next] == 0) {
+            q.offer(next);
           }
         }
       }
       int result = Integer.parseInt(br.readLine());
-      sb.append(DP[result]).append("\n");
+      System.out.println(DP[result]);
     }
-    System.out.println(sb);
   }
 }
