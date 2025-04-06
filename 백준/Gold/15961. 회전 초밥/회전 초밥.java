a34dc1fa02b45a435d0ce768a8c42d8c72619c70
dongschicken <138917930+dongschiken@users.noreply.github.com>
2025-04-06 12:30:08 +0900

    [Gold IV] Title: 회전 초밥, Time: 528 ms, Memory: 171124 KB -BaekjoonHub

---
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        int[] countArr = new int[d + 1];
+        int count = 0;
+        for(int i = 0; i < k; i++) {
+            if(countArr[arr[i]] == 0) count++;
+            countArr[arr[i]]++;
+        }
+        int max = count;
+        if(countArr[c] == 0) max++;
+        
+        for(int i = 0; i < N; i++) {
+            
+            countArr[arr[i]]--;
+            if(countArr[arr[i]] == 0) {
+                count--;
+            }
+            
+            if(countArr[arr[(i + k) % N]] == 0) {
+                count++;
+            }
+            countArr[arr[(i + k) % N]]++;
+            
+            if(countArr[c] == 0) {
+                max = Math.max(count + 1, max);
+            }else {
+                max = Math.max(count, max);
+            }
+        }
+        System.out.println(max);
+    }
+}
