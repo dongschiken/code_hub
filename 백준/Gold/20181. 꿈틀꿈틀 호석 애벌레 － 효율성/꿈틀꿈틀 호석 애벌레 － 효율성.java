976d8b273ce6418980559af7ce369038cf038c2a
dongschicken <138917930+dongschiken@users.noreply.github.com>
2025-04-21 23:53:05 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 232 ms, Memory: 26844 KB, Score: 31 point -BaekjoonHub

---
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] sum = new int[N + 1];
+        int[] DP = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        int l = 0;
+        int r = 1;
+        int cMax = 0;
+        while(l <= N && r <= N) {
+            int minus = sum[r] - sum[l];
+            
+            if(minus >= K) {
+                cMax = Math.max(cMax, DP[l]);
+                DP[r] = Math.max(cMax + minus - K, DP[r]);
+                l++;
+            }else {
+                r++;
+            }
+        }
+        System.out.println(DP[N]);
+    }
+}
