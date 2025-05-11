4dcfa29a6a52c40e2ea1c8698ea57777b4a2a107
dongschicken <138917930+dongschiken@users.noreply.github.com>
2025-05-11 23:40:19 +0900

    [Gold III] Title: 두 배열의 합, Time: 836 ms, Memory: 37312 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -8,12 +8,14 @@ public class Main {
         // A, B의 최대 부분합 = 499,500
         int N = Integer.parseInt(br.readLine());
         int[] a = new int[N];
+        // bsum, asum도 누적합 구할 때 int 초과할 수 있어서 long으로 선언
         long[] asum = new long[((N * (N - 1)) / 2) + N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
         int count = 0;
+        // 누적합
         for(int i = 0; i < N; i++) {
             long currentSum = 0;
             for(int j = i; j < N; j++) {
@@ -30,6 +32,7 @@ public class Main {
             b[i] = Integer.parseInt(st.nextToken());
         }
         count = 0;
+        // 누적합
         for(int i = 0; i < M; i++) {
             long currentSum = 0;
             for(int j = i; j < M; j++) {
@@ -39,6 +42,7 @@ public class Main {
         }
         Arrays.sort(bsum);
         
+        // 최대 결과가 500000 * 500000이면 int범위를 초과한다. -> long으로 선언
         long result = 0;
         for(int i = 0; i < asum.length; i++) {
             int lo = lowerbound(bsum, asum[i]);
