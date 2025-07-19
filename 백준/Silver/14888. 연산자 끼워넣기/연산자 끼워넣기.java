2cb5ed06be2f294c47a12386cf3b591ce3fd8e44
dongschicken <138917930+dongschiken@users.noreply.github.com>
2025-07-19 19:57:25 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 112 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -4,24 +4,16 @@ import java.util.*;
 public class Main {
     
     static int[] oCount = new int[4];
-    static char[] operators = new char[4];
-    static List<String> list = new ArrayList<>();
-    static int N;
-    static int M;
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
     static int[] nums;
+    static int N;
     
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        M = N - 1;
         nums = new int[N];
-        //operators[0] = '+';
-        //operators[1] = '-';
-        //operators[2] = '*';
-        //operators[3] = '/';
         
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
@@ -31,13 +23,13 @@ public class Main {
         for(int i = 0; i < 4; i++) {
             oCount[i] = Integer.parseInt(st.nextToken());
         }
-        dfs(0, nums[0]);
+        dfs(1, nums[0]);
         System.out.println(max);
         System.out.println(min);
     }
     
     public static void dfs(int stage, int value) {
-        if(stage == M) {
+        if(stage == N) {
             max = Math.max(max, value);
             min = Math.min(min, value);
             return;
@@ -49,16 +41,16 @@ public class Main {
                 oCount[i]--;
                 switch(i) {
                     case 0:
-                        dfs(stage + 1, value + nums[stage + 1]);
+                        dfs(stage + 1, value + nums[stage]);
                         break;
                     case 1:
-                        dfs(stage + 1, value - nums[stage + 1]);
+                        dfs(stage + 1, value - nums[stage]);
                         break;
                     case 2:
-                        dfs(stage + 1, value * nums[stage + 1]);
+                        dfs(stage + 1, value * nums[stage]);
                         break;
                     case 3:
-                        dfs(stage + 1, value / nums[stage + 1]);
+                        dfs(stage + 1, value / nums[stage]);
                         break;
                 }
                 oCount[i]++;
