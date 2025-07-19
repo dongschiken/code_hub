9a75b291ad5a1bcb690fb773d75d95890451f5ff
dongschicken <138917930+dongschiken@users.noreply.github.com>
2025-07-19 19:53:33 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 116 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -8,17 +8,21 @@ public class Main {
     static List<String> list = new ArrayList<>();
     static int N;
     static int M;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    static int[] nums;
     
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
         M = N - 1;
-        operators[0] = '+';
-        operators[1] = '-';
-        operators[2] = '*';
-        operators[3] = '/';
-        int[] nums = new int[N];
+        nums = new int[N];
+        //operators[0] = '+';
+        //operators[1] = '-';
+        //operators[2] = '*';
+        //operators[3] = '/';
+        
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
@@ -27,40 +31,15 @@ public class Main {
         for(int i = 0; i < 4; i++) {
             oCount[i] = Integer.parseInt(st.nextToken());
         }
-        dfs(0, "");
-        
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        
-        for(int i = 0; i < list.size(); i++) {
-            int temp = nums[0];
-            for(int j = 0; j < M; j++) {
-                // 0 1 1 2 2 3
-                switch(list.get(i).charAt(j)) {
-                    case '+':
-                        temp = temp + nums[j+1];
-                        break;
-                    case '-':
-                        temp = temp - nums[j+1];
-                        break;
-                    case '*':
-                        temp = temp * nums[j+1];
-                        break;
-                    case '/':
-                        temp = temp / nums[j+1];
-                        break;
-                }
-            }
-            max = Math.max(max, temp);
-            min = Math.min(min, temp);
-        }
+        dfs(0, nums[0]);
         System.out.println(max);
         System.out.println(min);
     }
     
-    public static void dfs(int stage, String str) {
+    public static void dfs(int stage, int value) {
         if(stage == M) {
-            list.add(str);
+            max = Math.max(max, value);
+            min = Math.min(min, value);
             return;
         }
         if(stage > N) return;
@@ -68,7 +47,20 @@ public class Main {
         for(int i = 0; i < 4; i++) {
             if(oCount[i] > 0) {
                 oCount[i]--;
-                dfs(stage + 1, str + operators[i]);
+                switch(i) {
+                    case 0:
+                        dfs(stage + 1, value + nums[stage + 1]);
+                        break;
+                    case 1:
+                        dfs(stage + 1, value - nums[stage + 1]);
+                        break;
+                    case 2:
+                        dfs(stage + 1, value * nums[stage + 1]);
+                        break;
+                    case 3:
+                        dfs(stage + 1, value / nums[stage + 1]);
+                        break;
+                }
                 oCount[i]++;
             }
         }
