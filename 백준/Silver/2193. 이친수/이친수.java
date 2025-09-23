26dfe565973645f7362d23abad801cb447129cd3
dongschicken <138917930+dongschiken@users.noreply.github.com>
2025-09-23 20:57:00 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] arr = new long[N + 3];
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i = 3; i < N; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        System.out.println(arr[N - 1]);
+    }
+}
